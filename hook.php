fe05759cd91e7110934140b710c876e0845e9069
Roman Gonyukov <roman@bx24.store>
2020-08-15 01:39:46 +0700

    удалил лишние комментарии

--- hook.php
+++ hook.php
@@ -1,30 +1,4 @@
 <?php
-/*
- -------------------------------------------------------------------------
- AdvTickets plugin for GLPI
- Copyright (C) 2020 by the AdvTickets Development Team.
-
- https://github.com/pluginsGLPI/advtickets
- -------------------------------------------------------------------------
-
- LICENSE
-
- This file is part of AdvTickets.
-
- AdvTickets is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- AdvTickets is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with AdvTickets. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- */
 
 /**
  * Plugin install process
@@ -57,6 +31,9 @@ function plugin_advtickets_item_add_ticket(Ticket $item) {
     Adv\Event::getInstance()->handleNewTicket($item);
 }
 
+/**
+ * @param Ticket $item
+ */
 function plugin_advtickets_item_update_ticket(Ticket $item)
 {
     plugin_advticket_autoload_classes();
