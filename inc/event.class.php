3ba0a0cc9bb173c9d35aa85405ecef6f3962e52e
Roman Gonyukov <roman@bx24.store>
2020-08-17 14:40:57 +0700

    удалил лишний код из метода

--- inc/event.class.php
+++ inc/event.class.php
@@ -6,41 +6,36 @@ class PluginAdvticketsEvent extends CommonDBTM
     static function pre_item_add_ticket(Ticket $ticket)
     {
         global $DB;
+
         $fields = $ticket->input;
 
         if($fields['_link']['tickets_id_2']) {
 
             $relatedTicketId = $fields['_link']['tickets_id_2'];
 
-            $relatedTicket = $DB->request('glpi_tickets', ['id' => $relatedTicketId]);
+            $relatedTicketParamsForUpdate = [
+                'itemtype' => \Ticket::class,
+                'items_id' => $relatedTicketId,
+                'users_id' => $fields['_users_id_requester'],
+                'users_id_editor' => 0,
+                'content' => $fields['content'],
+                'date' => date('c'),
+                'date_mod' => date('c'),
+                'date_creation' => date('c'),
+                'is_private' => 0,
+                'requesttypes_id' => $fields['requesttypes_id'],
+                'timeline_position' => 4,
+                'sourceitems_id' => 0,
+                'sourceof_items_id' => 0
+            ];
 
-            foreach ($relatedTicket as $rTicketId => $rTicketRow) {
+            $DB->insert('glpi_itilfollowups', $relatedTicketParamsForUpdate);
 
-                $relatedTicketParamsForUpdate = [
-                    'itemtype' => \Ticket::class,
-                    'items_id' => $relatedTicketId,
-                    'users_id' => $fields['_users_id_requester'],
-                    'users_id_editor' => 0,
-                    'content' => $fields['content'],
-                    'date' => date('c'),
-                    'date_mod' => date('c'),
-                    'date_creation' => date('c'),
-                    'is_private' => 0,
-                    'requesttypes_id' => $fields['requesttypes_id'],
-                    'timeline_position' => 4,
-                    'sourceitems_id' => 0,
-                    'sourceof_items_id' => 0
-                ];
-
-                $DB->insert('glpi_itilfollowups', $relatedTicketParamsForUpdate);
-
-                $DB->update('glpi_tickets', [
-                    'status' => 1
-                ], [
-                    'id' => $relatedTicketId
-                ]);
-
-            }
+            $DB->update('glpi_tickets', [
+                'status' => 1
+            ], [
+                'id' => $relatedTicketId
+            ]);
 
             $ticket->input = false;
 
